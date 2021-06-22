afc4575f5d25e1e3f502efdc17c43b6497d7ab03
Vitaliy Parashchak <paraschak@gmail.com>
2021-06-22 08:57:04 +0300

    Move NSUIColor extension from Platforms.swift to LineChartRenderer.swift (otherwise not possible to build the app with pod)

--- Source/Charts/Renderers/LineChartRenderer.swift
+++ Source/Charts/Renderers/LineChartRenderer.swift
@@ -931,3 +931,23 @@ open class LineChartRenderer: LineRadarRenderer
         return element
     }
 }
+
+extension NSUIColor
+{
+    var nsuirgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)? {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+
+        guard let colorSpaceModel = cgColor.colorSpace?.model else {
+            return nil
+        }
+        guard colorSpaceModel == .rgb else {
+            return nil
+        }
+
+        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        return (red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
